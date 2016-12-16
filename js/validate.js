23cedfbb0fde61036679d6f71b857b69f8a5f937
Lesley <lchan5@users.noreply.github.com>
2016-12-16 00:59:39 -0500

    Update validate.js

--- js/validate.js
+++ js/validate.js
@@ -42,7 +42,11 @@
     		$.ajax({
       		  url: "//formspree.io/lchan5@gmail.com", 
       		  method: "POST",
-      		  data: $(form).serialize(),
+      		  data: {
+                 name: $(form).find("input[name='name']").val(),
+                _replyto: $(form).find("input[name='_replyto']").val(),
+                message: $(form).find("textarea[name='message']").val() 
+              },
       		  dataType: "json",
               success: function() {
        		 	  $("#submit-success").fadeIn();
