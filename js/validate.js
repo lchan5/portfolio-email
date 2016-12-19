8db689c514894604783bec20f7baf2d146fd4ab1
Lesley <lchan5@users.noreply.github.com>
2016-12-18 19:52:12 -0500

    Update validate.js

--- js/validate.js
+++ js/validate.js
@@ -1,43 +1,6 @@
 // validate contact form
     $('#contact').validate({
-        rules: {
-            name: {
-                required: true,
-                minlength: 2
-            },
-            email: {
-                required: true,
-                email: true
-            },
-            message: {
-                required: true
-            }
-            /*
-            answer: {
-                required: true,
-                answercheck: true
-            }
-            */
-        },
-        messages: {
-            name: {
-                required: "<br />this field is required<br /><br />",
-                minlength: "your name must consist of at least 2 characters"
-            },
-            email: {
-                required: "<br />this field is required<br /><br />"
-            },
-            message: {
-                required: "<br />this field is required<br /><br />",
-                minlength: "thats all? really?"
-            }
-            /*
-            answer: {
-                required: "<br />sorry, wrong answer!"
-            }
-            */
-        },
-        
+
         submitHandler: function(form) {
     		$.ajax({
                 url: "//formspree.io/lchan5@gmail.com",
@@ -45,19 +8,22 @@
                 data: $(form).serialize(),
                 dataType: "json",
                 success: function() {
-                    $("#submit-success").fadeIn();
-                    $("#contactform").fadeOut();
+                    $('#contactform :input').attr('disabled', 'disabled');
+                    $('#contactform').fadeTo( "slow", 0.15, function() {
+                        $(this).find(':input').attr('disabled', 'disabled');
+                        $(this).find('label').css('cursor','default');
+                        $('#submit-success').fadeIn();
+                    });
                 },
                 error: function() {
-                    $("#submit-errors").fadeIn();
+                    $('#contactform').fadeTo( "slow", 0.15, function() {
+                        $('#submit-errors').fadeIn();
+                    });
                 }
             });
     	    
   	    }
-        
-        
-        
-        
+          
         
         
     });
